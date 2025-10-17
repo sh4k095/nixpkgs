6973b6c137497d5c61ea8eb81766d609503d427f
sh4k0 <carlofedep@gmail.com>
2025-10-17 10:33:07 +0200

    python312Packages.ovito: init at 3.14.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11327,6 +11327,8 @@ self: super: with self; {
 
   ovh = callPackage ../development/python-modules/ovh { };
 
+  ovito = callPackage ../development/python-modules/ovito { };
+
   ovmfvartool = callPackage ../development/python-modules/ovmfvartool { };
 
   ovoenergy = callPackage ../development/python-modules/ovoenergy { };
